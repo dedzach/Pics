0dbfe33d0cbed8c56cde4fa78cfce1709779f931
dedzach <zacharymoh11@gmail.com>
2021-10-25 10:53:34 -0500

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+class SearchBar extends React.Component {
+    render () {
+        return <div>Search Bar</div>;
+    }
+}
+
+export default SearchBar;
