9d865b301a2e9981e8261eea04d3ff5ed845b8f1
Zachary Moh <zacharymoh11@gmail.com>
2021-10-26 12:08:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
       params: { query: term },
       headers: {
         Authorization:
-          'Client-ID DvjbVHW1IKXrBuY8QKT9bqRFGjV4266BhjaDlYTOV9k',
+          'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',
       },
     });
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDom from 'react-dom';
 
 class SearchBar extends React.Component {
+    state = { term: ''};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    }
+
     render () {
-        return <div>Search Bar</div>;
+        return (
+        <div className="ui segment">
+            <form onSubmit={this.onFormSubmit} className="ui form">
+                <div className="field">
+                    <label>Image Search</label>
+                    <input type="text" value=   {this.state.term} onChange={ e => this.setState({ term: e.target.value })}/>
+                </div>
+            </form>
+        </div>
+        );
     }
 }
 
