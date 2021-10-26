116a171d28ddd0c62f3fae54066e5b758727f61b
dedzach <zacharymoh11@gmail.com>
2021-10-26 16:42:08 -0500

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        this.props.onSubmit(this.state.term);
+        this.props.onSearchSubmit(this.state.term);
     }
 
     render () {
