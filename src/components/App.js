a324aa66ee1917c18c7c6a07bd6f789238ac5628
Zachary Moh <zacharymoh11@gmail.com>
2021-10-26 20:17:18 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+import './ImageList.css'
 
 class App extends React.Component {
     state = { images: [] };
@@ -20,7 +22,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
